43bbfadf1091d597dd71444326eb673989da84e7
shashankssp12 <shashankspandey12@gmail.com>
2025-03-13 11:18:26 +0530

    changes

--- generateKey.py
+++
@@ -1,4 +0,0 @@
-from django.core.management.utils import get_random_secret_key
-print(get_random_secret_key())
-# or run this command in terminal
-# python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'
