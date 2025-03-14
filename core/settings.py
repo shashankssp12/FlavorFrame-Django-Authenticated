85b30eda0373d14a040721ada64f7ba351d1a848
shashankssp12 <shashankspandey12@gmail.com>
2025-03-14 20:35:51 +0530

    db for locally running the application

--- core/settings.py
+++ core/settings.py
@@ -95,6 +95,14 @@ if 'VERCEL' in os.environ:
             'PORT': os.environ.get('DB_PORT', '5432'),
         }
     }
+else:
+    # Use SQLite for local development
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
